e68b9b05641efc4b2c4d15e7d35f090771ab61e6
Martin Fernandez <Electronicsmf@aol.com>
2022-10-22 21:09:20 -0400

    adding items to cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/_services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+  public products: any = [];
+  public grandTotal: number = 0;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cartService.getProducts().subscribe(res => {
+      this.products = res;
+      this.grandTotal = this.cartService.getTotalPrice();
+    })
+  }
+
+
+  removeItem(item: any) {
+    this.cartService.removeCartItem(item);
+  }
+
+  emptyCart() {
+    this.cartService.removeAllCart();
   }
 
 }
